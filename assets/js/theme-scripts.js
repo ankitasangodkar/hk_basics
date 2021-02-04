02cd30a7ec3503ab5e87c6417e5f0e7ab09ed896
Ankita <ankita@ajency.in>
2021-02-04 17:16:58 +0530

    product details sections

--- assets/js/theme-scripts.js
+++ assets/js/theme-scripts.js
@@ -4,8 +4,16 @@ $( document ).ready(function() {
     if($(this).attr('href')){
       alert('redirect to '+$(this).attr('href'));
       window.location.replace($(this).attr('href'));
-      
        }
-    
   });
+});
+
+$(document).on('show','.accordion', function (e) {
+     //$('.accordion-heading i').toggleClass(' ');
+     $(e.target).prev('.accordion-heading').addClass('accordion-opened');
+});
+
+$(document).on('hide','.accordion', function (e) {
+    $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
+    //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
 });
