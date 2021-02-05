0bd83d400d9f9944625c5368b70ad412782bcc23
Ankita <ankita@ajency.in>
2021-02-05 10:40:45 +0530

    product details desktop fixes

--- assets/js/theme-scripts.js
+++ assets/js/theme-scripts.js
@@ -17,3 +17,45 @@ $(document).on('hide','.accordion', function (e) {
     $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
     //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
 });
+
+
+$('.product-slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  centerPadding: '60px',
+  prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa fa-arrow-right"></i></button>',
+  nextArrow: '<button class="slide-arrow next-arrow"><i class="fa fa-arrow-left"></i></button>',
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true,
+    }
+  }
+  ]
+});
+
+$('.product-image-slider').slick({
+  dots: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa fa-arrow-right"></i></button>',
+  nextArrow: '<button class="slide-arrow next-arrow"><i class="fa fa-arrow-left"></i></button>',
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+    }
+  }
+  ]
+});
